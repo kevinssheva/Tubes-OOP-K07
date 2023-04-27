3782e03d0bda6acc697e90447151bdf4c63bc0a8
Aufar R <31300357+AufarR@users.noreply.github.com>
2023-04-27 15:44:02 +0700

    Update Stove.java -> Cooking action
    Pending action conversion to runnable/thread

--- src/Objek/Furniture/Stove.java
+++ src/Objek/Furniture/Stove.java
@@ -7,9 +7,20 @@ public class Stove extends Furniture {
     public Stove(String name, Integer price) {
         super(name, "Memasak", price);
     }
-
-    public void Aksi(Sim a) // gotta make a food class first
+    
+    // Blm dibikin thread yh huehue (contoh deng mwehehe)
+    public void aksi(Sim s, Dish d) throws Exception
     {
-
+        for (Ingredients ing: d.getListIngredients()) {
+            if (!(s.checkInventory(ing))) {
+                throw new Exception("Dish ingredients not sufficient!");
+            }
+        }
+        s.setStatus("Cooking....");
+        for (Ingredients ing: d.getListIngredients()) {
+            s.deleteFromInventory(ing);
+        }
+        s.addToInventory(d);
+        s.setStatus("Idle");
     }
 }
