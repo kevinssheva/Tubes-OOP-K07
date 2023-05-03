a5d28c7332764853e1305a2e7d2230b3269b35f9
Aufar R <31300357+AufarR@users.noreply.github.com>
2023-05-03 11:57:33 +0700

    Create Piano.java

---
+++ src/Objek/Furniture/Piano.java
@@ -0,0 +1,11 @@
+package src.Objek.Furniture;
+
+import java.awt.Dimension;
+
+import src.Sim.Sim;
+
+public class Piano extends Furniture {
+    public Piano() {
+        super("Piano", "Main piano", 30, new Dimension(2, 1));
+    }
+}
