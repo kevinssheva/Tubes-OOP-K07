9a2c2ff0512120c5e910e6518f2c8a0f2cf4246f
Aufar R <31300357+AufarR@users.noreply.github.com>
2023-05-03 12:13:20 +0700

    Update Piano.java > edit action description

--- src/Objek/Furniture/Piano.java
+++ src/Objek/Furniture/Piano.java
@@ -6,6 +6,6 @@ import src.Sim.Sim;
 
 public class Piano extends Furniture {
     public Piano() {
-        super("Piano", "Main piano", 30, new Dimension(2, 1));
+        super("Piano", "Play piano", 30, new Dimension(2, 1));
     }
 }
