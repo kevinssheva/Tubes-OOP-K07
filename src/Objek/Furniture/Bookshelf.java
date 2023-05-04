25eddcb9dfad3f31c290b3bc997fbc9dbe4ceb9a
Aufar R <31300357+AufarR@users.noreply.github.com>
2023-05-04 16:58:32 +0700

    Update and rename RakBuku.java to Bookshelf.java

---
+++ src/Objek/Furniture/Bookshelf.java
@@ -0,0 +1,9 @@
+package src.Objek.Furniture;
+
+import java.awt.Dimension;
+
+public class Bookshelf extends Furniture {
+    public Bookshelf() {
+        super("Bookshelf", "Read books", 25, new Dimension(3, 1));
+    }
+}
--- src/Objek/Furniture/RakBuku.java
+++
@@ -1,9 +0,0 @@
-package src.Objek.Furniture;
-
-import java.awt.Dimension;
-
-public class RakBuku extends Furniture {
-    public RakBuku() {
-        super("Rak Buku", "Baca buku", 25, new Dimension(3, 1));
-    }
-}
