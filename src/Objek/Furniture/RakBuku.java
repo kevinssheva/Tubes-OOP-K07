03cf9e14475f8ee57725bafba47e62152e42df63
Aufar R <31300357+AufarR@users.noreply.github.com>
2023-05-04 16:56:02 +0700

    Create furniture RakBuku.java

---
+++ src/Objek/Furniture/RakBuku.java
@@ -0,0 +1,9 @@
+package src.Objek.Furniture;
+
+import java.awt.Dimension;
+
+public class RakBuku extends Furniture {
+    public RakBuku() {
+        super("Rak Buku", "Baca buku", 25, new Dimension(3, 1));
+    }
+}
